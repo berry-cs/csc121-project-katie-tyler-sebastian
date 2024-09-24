c693211ce29eda34c295f785ad2489f7ff5b97b8
sebastiannivong <sebastiannivong@gmail.com>
2024-09-24 19:44:08 -0400

    Create Posn.java

---
+++ csc121-creative-project/src/Posn.java
@@ -0,0 +1,29 @@
+
+public class Posn {
+    int x;
+    int y;
+
+
+public Posn(int x, int y) {
+	this.x = x;
+	this.y = y;
+	}
+
+/**
+ *  moves the snake forward and backward
+ * 
+ */
+
+public int moveX() {
+	
+	return x;
+}
+
+/**
+ *  move the snake up and down
+ */
+public int moveY() {
+	return y;
+}
+
+}
