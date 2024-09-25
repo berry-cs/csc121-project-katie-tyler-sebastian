f9a02994289ab58e752a2e6018b3fcf4efef70c9
sebastiannivong <sebastiannivong@gmail.com>
2024-09-24 20:05:27 -0400

    Create Apple.java

---
+++ csc121-creative-project/src/Apple.java
@@ -0,0 +1,15 @@
+/**
+ * Class for Apple for snake 
+ */
+
+
+public class Apple {
+ Posn posn;
+ boolean eaten;
+ 
+ public Apple(Posn posn, boolean eaten) {
+ this.posn = posn;
+ this.eaten = eaten;
+ }
+}
+
