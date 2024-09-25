623ef6e1024af80b3dab733631b65f62f4d11fa8
katiealoe <katieealon@gmail.com>
2024-09-24 20:17:26 -0400

    Update AppleWorld.java

--- csc121-creative-project/src/AppleWorld.java
+++ csc121-creative-project/src/AppleWorld.java
@@ -1,9 +1,11 @@
+import processing.core.PApplet;
+
 /**
  * Class for Apple for snake 
  */
 
 
-public class AppleWorld {
+public class AppleWorld implements IWorld {
  Posn posn;
  boolean eaten;
  
@@ -11,5 +13,16 @@ public class AppleWorld {
  this.posn = posn;
  this.eaten = eaten;
  }
+ 
+ public PApplet draw(PApplet w) { 
+		w.circle(this.posn.x, this.posn.y, 10);
+		return w;
+		}
+	
+	public IWorld update(SnakeWorld s) {
+		if (this.posn.x == s.posn.x && this.posn.y == s.posn.y) {
+			return new AppleWorld(new Posn(this.posn.x, this.posn.y), true);
+		} else {return this;}
+	}
 }
 
